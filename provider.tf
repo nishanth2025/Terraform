5cb05b9499839d748caefa948a9af8bd53404bc7
Nishanth R <VDELT107@Nishanth-Mac.local>
2020-01-28 11:51:53 +0530

    terraform files

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws"{
+region = "${var.region}"
+access_key = "${var.access}"
+secret_key = "${var.secret}"
+}
+
---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "region"{
+	type = "string"
+	default = "us-east-1"
+}
+variable "access"{
+	type = "string"
+	default = "AKIAJFTT6K7T7IIBM6MQ"
+}
+variable  "secret"
+	type ="string"
+	default = "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"
+}
+variable "vpc-cidr"{
+	type = "string"
+	default = "10.0.0.0/26"
+}
+
